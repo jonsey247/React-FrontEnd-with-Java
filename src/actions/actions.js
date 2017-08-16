ec665a2ab5aa27efe2a95b08dc5e8c68060e6373
jonsey247 <jacobfilmjones@hotmail.com>
2017-08-16 11:04:57 +0100

    config, css, actions, types. added

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,10 +2,6 @@ import * as types from './types';
 import axios from 'axios';
 import { ROOT } from '../../config';
 
-// tester!
-export function hiGuys() {
-    console.log('Hello');
-}
 
 // fetchTvs
 export function fetchTvs() {
@@ -13,8 +9,7 @@ export function fetchTvs() {
         dispatch(fetchTvsRequest());
         axios.get(`${ROOT}/tvs`)
             .then(res => {
-                console.log(res.data);
-                dispatch(fetchTvsSuccess(res.data.tvs));
+                dispatch(fetchTvsSuccess(res.data));
             })
             .catch(err => {
                 console.log(err);
@@ -36,9 +31,83 @@ export function fetchTvsSuccess(tvs) {
     };
 }
 
-export function fetchArticlesError(error) {
+export function fetchTvsError(error) {
     return {
         type: types.FETCH_TVS_ERROR,
+        payload: error
+    };
+}
+
+// fetchTvsByID
+export function fetchTvsByID(id) {
+    return function (dispatch) {
+        dispatch(fetchTvsByIDRequest(id));
+        axios.get(`${ROOT}/tvs/${id}`)
+            .then(res => {
+                dispatch(fetchTvsByIDSuccess(res.data));
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch(fetchTvsByIDError(err));
+            });
+    };
+}
+
+export function fetchTvsByIDRequest(id) {
+    return {
+        type: types.FETCH_TVS_BY_ID_REQUESTS,
+        id: id
+    };
+}
+
+export function fetchTvsByIDSuccess(tv) {
+    return {
+        type: types.FETCH_TVS_BY_ID_SUCCESS,
+        payload: tv
+    };
+}
+
+export function fetchTvsByIDError(error) {
+    return {
+        type: types.FETCH_TVS_BY_ID_ERROR,
+        payload: error
+    };
+}
+
+// addnewTv
+export function addTv(data) {
+    return function (dispatch) {
+        dispatch(addTvPost(data));
+        axios.post(`${ROOT}/tvs/-1`, data)
+            .then(res => {
+                console.log('new tv res: ', res);
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(addTvError(err));
+            });
+    };
+}
+
+export function addTvPost(tvModel, wattage) {
+    return {
+        type: types.ADD_TV_POST,
+        tvModel: tvModel,
+        wattage: wattage
+    };
+}
+
+// Function is not needed at this time
+// export function addTvSuccess(newtv) {
+//     return {
+//         type: types.ADD_TV_SUCCESS,
+//         payload: newTv
+//     };
+// }
+
+export function addTvError(error) {
+    return {
+        type: types.ADD_TV_ERROR,
         payload: error
     };
 }
